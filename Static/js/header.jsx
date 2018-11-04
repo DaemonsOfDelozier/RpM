b0d6aaefb133289b11925e5621554d9836a9b9f7
Clay James <clayw.james@gmail.com>
2018-11-03 20:48:00 -0400

    fix header

--- Static/js/header.jsx
+++ Static/js/header.jsx
@@ -5,14 +5,29 @@ export default class Header extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            isMobile: window.innerWidth < 992
+        }
+    }
+
+    renderLogo() {
+        if (this.state.isMobile) return null;
+        return (
+            <a className="navbar-brand js-scroll-trigger" href="#page-top">
+                <div className="logo-container">
+                    <img src="../dist/css/img/rpm-logo.png"/>
+                </div>
+            </a>
+        ); 
     }
     
     render() {
+        const signInStyle = this.state.isMobile ? {} : {marginLeft: "150px"};
         return (
             <nav className="navbar navbar-expand-lg fixed-top text-uppercase" id="mainNav">
-                <div className="container">
-                    
-                    <a className="navbar-brand js-scroll-trigger" href="#page-top"><img src="../dist/css/img/rpm-logo.png"/></a>
+                <div className="container" style={{maxHeight: "75px", maxWidth: "100%", marginLeft: "0px"}}>
+                    {this.renderLogo()}
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul id="menu" className="navbar-nav">
                             <li className="nav-item mx-0 mx-lg-1">
@@ -29,7 +44,9 @@ export default class Header extends React.Component {
                             </li>
                         </ul>
                     </div>
-                    <SignIn user={this.props.user}></SignIn>
+                    <div style={signInStyle}>
+                        <SignIn user={this.props.user}></SignIn>
+                    </div>
                 </div>
             </nav>
         );
