c59373542b5d493aa3596159e4192b029a3a73bc
Justin <mckennaman123@gmail.com>
2018-11-05 17:49:56 -0500

    changing options in travis yml instead of setting them in each test

--- Tests/features/steps/google_signin.py
+++ Tests/features/steps/google_signin.py
@@ -4,12 +4,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
 
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-
 @given(u'we click on google sign in button')
 def step_impl(context):
-	browser = webdriver.Chrome(chrome_options=chrome_options)
+	browser = webdriver.Chrome()
 	context.browser = browser
 	browser.get('localhost:5000')
 	browser.maximize_window()
