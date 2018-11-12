d7bb0f1794bbd401dfd98428c2bd3ef1e94a1b39
mckennaman123 <mckennaman123@gmail.com>
2018-11-12 17:20:28 -0500

    help changed to about us, so test asserts that. still trying to figure out how to emulate a phone using webdriver without just changing the browser size

--- Tests/features/steps/navagation_bar.py
+++ Tests/features/steps/navagation_bar.py
@@ -30,10 +30,10 @@ def step_impl(context):
 @then(u'We want to see RPM, Explore, Route Map, Help, and Account')
 def step_impl(context):
 	browser = context.browser
-	rpm_button = browser.find_element_by_xpath('//*[@id="mainNav"]/div/a')
+	rpm_button = browser.find_element_by_xpath('//*[@id="logo"]/div[2]/img')
 	assert 'Explore' in browser.page_source
 	assert 'Route Map' in browser.page_source
-	assert 'Help' in browser.page_source
+	assert 'About Us' in browser.page_source
 	assert 'Account' in browser.page_source
 	browser.quit()
 	
@@ -45,23 +45,23 @@ def step_impl(context):
 	#access_key = os.environ["SAUCE_ACCESS_KEY"]
 	#caps = {'browserName': 'Chrome',
 	#	    'version': '60.0',
-	#	    'tunnel-identifier': os.environ["TRAVIS_JOB_NUMBER"]}
+	#	    'tunnel-identifier': os.environ["TRAVIS_JOB_NUMBER"]
+	#		'mobile_emulation': }
 	#browser = webdriver.Remote(
     #	desired_capabilities= caps,
     #	command_executor='http://%s:%s@ondemand.saucelabs.com:80/wd/hub' % (username, access_key)
 	#)
 
 	#for local testing
-	browser = webdriver.Chrome()
+	browser = webdriver.Chrome(mobile_emulation = { deviceName: "iPhone X" })
 
 	context.browser = browser
 	browser.get('localhost:5000')
-	browser.set_window_size(375, 812)
 
 @when(u'We click the menu button')
 def step_impl(context):
 	browser = context.browser
-	menu_button = browser.find_element_by_xpath('/html/body/div[1]/a')
+	menu_button = browser.find_element_by_xpath('//*[@id="menu"]')
 	menu_button.click()
 	time.sleep(1)
 
