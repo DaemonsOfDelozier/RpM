49a8822c45945055830972d6d52b8617a5175586
Clay James <clayw.james@gmail.com>
2018-11-16 00:04:37 -0500

    Change styles to include user who posted

--- Static/js/Components/post.jsx
+++ Static/js/Components/post.jsx
@@ -6,7 +6,7 @@ import StarRatingComponent from 'react-star-rating-component';
 const metersPerMile = 1609.344;
 
 export default class Post extends React.Component {
-    
+
     constructor(props) {
         super(props);
 
@@ -17,11 +17,11 @@ export default class Post extends React.Component {
 
         this.onResponse = this.onResponse.bind(this);
     }
-    
+
     renderDistaceDuration() {
         if (!this.state.distance || !this.state.duration) return null;
         return (
-            <p className="emphasized" style={{marginBottom: "0px"}}>{this.state.distance} - {this.state.duration}</p>
+            <p className="emphasized" style={{ marginBottom: "0px" }}>{this.state.distance} - {this.state.duration}</p>
         );
     }
 
@@ -38,6 +38,30 @@ export default class Post extends React.Component {
         );
     }
 
+    renderUserLocationRating(post) {
+        return (
+            <Grid item sm={12}
+                container direction="row" justify="space-between" alignItems="flex-start">
+
+                <Grid item md={12} lg={6} container direction="row" justify="space-between" alignItems="flex-start">
+                    <Grid item sm={6}>
+                        <h5>{post.userid}</h5>
+                    </Grid>
+                    <Grid item sm={6}>
+                        <h5 style={{ float: "right" }}>{post.start}</h5>
+                    </Grid>
+                </Grid>
+                <Grid item md={12} lg={6}>
+                    <StarRatingComponent name={post.id + "_rating"}
+                        starCount={5}
+                        value={post.rating}
+                        editing={false} />
+                </Grid>
+
+            </Grid>
+        );
+    }
+
     onResponse(response) {
         const legs = response.routes[0].legs;
 
@@ -46,7 +70,7 @@ export default class Post extends React.Component {
 
         const seconds = legs.reduce((acc, current) => acc + current.duration.value, 0);
         const hours = Math.floor(seconds / 3600);
-        const minutes = Math.round((seconds - (hours*3600)) / 60)
+        const minutes = Math.round((seconds - (hours * 3600)) / 60)
 
         this.setState({
             distance: `${miles} miles`,
@@ -58,34 +82,28 @@ export default class Post extends React.Component {
         const post = this.props.post;
         return (
             <div style={{ padding: "20px" }}>
-                <h3 className="emphasized">{post.title}</h3>
-                <Grid container direction="row" justify="flex-start" alignItems="flex-start" spacing={24}>
+                <Grid container direction="row" justify="flex-start" alignItems="flex-start">
+                    {this.renderUserLocationRating(post)}
                     <Grid item md={12} lg={6}>
-                        <Grid container direction="row" justify="space-between" alignItems="flex-start" spacing={24} item sm={12}>
-                            <Grid item sm={6}><h5>{post.start}</h5></Grid>
-                            <Grid item sm={6}>
-                                <StarRatingComponent name={post.id + "_rating"}
-                                                     starCount={5}
-                                                     value={post.rating}
-                                                     editing={false}/>
-                            </Grid>
-                        </Grid>
-                        <Map id={post.id} 
-                             start={post.start} 
-                             end={post.end} 
-                             waypoints={post.waypoints}
-                             onResponse={this.onResponse} />
+                        <Map id={post.id}
+                            start={post.start}
+                            end={post.end}
+                            waypoints={post.waypoints}
+                            onResponse={this.onResponse} />
                     </Grid>
-                    <Grid item zeroMinWidth md={12} lg={6} style={{ paddingTop: "40px" }}>
-                        <h4 className="emphasized">Description</h4>
-                        <p>{post.description}</p>
-                        <br />
-                        <h4 className="emphasized">Route</h4>
-                        {this.renderDistaceDuration()}
-                        {this.renderDirections()}
-                        <br />
-                        <h4 className="emphasized">Notes</h4>
-                        <p>{post.notes}</p>
+                    <Grid item zeroMinWidth md={12} lg={6}>
+                        <div style={{ padding: "20px", paddingTop: "0px" }}>
+                            <h4 className="emphasized">{post.title}</h4>
+                            <p>{post.description}</p>
+                            <br />
+                            <h4 className="emphasized">Route</h4>
+                            {this.renderDistaceDuration()}
+                            {this.renderDirections()}
+                            <br />
+                            <h4 className="emphasized">Notes</h4>
+                            <p>{post.notes}</p>
+                        </div>
+
                     </Grid>
                 </Grid>
             </div>
