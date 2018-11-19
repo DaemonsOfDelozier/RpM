72acc1a0c4b1788604b88a7b67880e55920c58cb
mckennaman123 <mckennaman123@gmail.com>
2018-11-18 19:25:41 -0500

    added mobile tests to nav bar tests and google sign in test

--- Tests/features/google_signin.feature
+++ Tests/features/google_signin.feature
@@ -5,3 +5,9 @@ Feature: google sign in
         When  we sign in to google
 	Then  we should see our name on the nav bar
 	And   we should be able to log out afterwards  
+
+   Scenario: Sign in and out on a mobile device
+        Given we click on google sign in button on mobile
+        When  we sign in to google on mobile
+        Then  we should see our name on the mobile nav bar
+        And   we should be able to log out on mobile
--- Tests/features/steps/google_signin.py
+++ Tests/features/steps/google_signin.py
@@ -5,20 +5,10 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import os
 
+#Sign in and out on desktop
 @given(u'we click on google sign in button')
 def step_impl(context):
-	#setting up remote browser
-	#username = os.environ["SAUCE_USERNAME"]
-	#access_key = os.environ["SAUCE_ACCESS_KEY"]
-	#caps = {'browserName': 'Chrome',
-	#	    'version': '60.0',
-	#	    'tunnel-identifier': os.environ["TRAVIS_JOB_NUMBER"]}
-	#browser = webdriver.Remote(
-    #	desired_capabilities= caps,
-    #	command_executor='http://%s:%s@ondemand.saucelabs.com:80/wd/hub' % (username, access_key)
-	#)
-
-	#for local testing
+	#local testing
 	browser = webdriver.Chrome()
 	
 	#tests start now
@@ -32,37 +22,24 @@ def step_impl(context):
 def step_impl(context):
 	browser = context.browser
 	
-	#MOST RECENT UPDATE
-	#So google is now throwing different sign in screens at us
-	#this never happened during local testing but started after using travis
-	#this also changes the exact xpath of the email/password input
-	#boxes, so to conclude: we cant beat google; this test is useless
-	google_beat_us = True
 	sign_in_window = browser.window_handles[1]
 	browser.switch_to_window(sign_in_window)
-	time.sleep(1)
+	time.sleep(3)
 	
-	#entering email and password
-	if (not(google_beat_us)):
-		email_text_box = browser.find_element_by_xpath('//*[@id="identifierId"]')
-		email_text_box.send_keys('testingiscool86@gmail.com')
-		email_next_button = browser.find_element_by_xpath('//*[@id="identifierNext"]')
-		email_next_button.click()
-		time.sleep(1)
-	
-	#checks if google wants us to type a captcha in
-	#if this happens we cant continue the scenario
-	if ('Type the text you hear or see' in browser.page_source):
-		google_beat_us = True
-	if (not(google_beat_us)):
-		password_text_box = browser.find_element_by_xpath('//*[@id=\"password\"]/div[1]/div/div[1]/input')
-		password_text_box.send_keys('testing4lyfe')
-		password_next_button = browser.find_element_by_xpath('//*[@id="passwordNext"]')
-		password_next_button.click()
-		time.sleep(1)
+	email_text_box = browser.find_element_by_xpath('//*[@id="identifierId"]')
+	email_text_box.send_keys('testingiscool86@gmail.com')
+	email_next_button = browser.find_element_by_xpath('//*[@id="identifierNext"]')
+	email_next_button.click()
+	time.sleep(3)
+	password_text_box = browser.find_element_by_xpath('//*[@id=\"password\"]/div[1]/div/div[1]/input')
+	password_text_box.send_keys('testing4lyfe')
+	password_next_button = browser.find_element_by_xpath('//*[@id="passwordNext"]')
+	password_next_button.click()
+	time.sleep(5)
 
 	#checks if google wants us to verify via phone number
 	#if this happens we cant continue the scenario
+	google_beat_us = False
 	if (len(browser.window_handles) == 2): #if the sign in window is still open
 		google_beat_us = True
 	context.google_beat_us = google_beat_us
@@ -77,6 +54,7 @@ def step_impl(context):
 		browser.switch_to_window(original_window)
 		upper_right_text = browser.find_element_by_xpath('//*[@id="mainNav"]/div/div[2]/div').text
 		assert upper_right_text == 'JOHN DOELogout'
+		time.sleep(3)
 	else:
 		#closing sign in
 		browser.close()
@@ -85,6 +63,77 @@ def step_impl(context):
 		context.browser = browser
 
 @then(u'we should be able to log out afterwards')
+def step_impl(context):
+	google_beat_us = context.google_beat_us
+	browser = context.browser
+	if(not(google_beat_us)):
+		#making sure we logged out
+		logout_button = browser.find_element_by_xpath('//*[@id="mainNav"]/div/div[2]/div/button')
+		logout_button.click()
+		assert not('JOHN DOE' in browser.page_source)
+		time.sleep(1)
+	browser.quit()
+
+#Sign in and out on mobile
+@given(u'we click on google sign in button on mobile')
+def step_impl(context):
+	#local testing
+	mobile_emulation = { "deviceName": "iPhone X" }
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+	browser = webdriver.Chrome(desired_capabilities = chrome_options.to_capabilities())
+
+	context.browser = browser
+	browser.get('localhost:5000')
+
+	sign_in_button = browser.find_element_by_xpath('//*[@id="mainNav"]/div/div[2]/button')
+	sign_in_button.click()
+
+@when(u'we sign in to google on mobile')
+def step_impl(context):
+	browser = context.browser
+	
+	sign_in_window = browser.window_handles[1]
+	browser.switch_to_window(sign_in_window)
+	time.sleep(3)
+	
+	email_text_box = browser.find_element_by_xpath('//*[@id="identifierId"]')
+	email_text_box.send_keys('testingiscool86@gmail.com')
+	email_next_button = browser.find_element_by_xpath('//*[@id="identifierNext"]')
+	email_next_button.click()
+	time.sleep(3)
+	password_text_box = browser.find_element_by_xpath('//*[@id=\"password\"]/div[1]/div/div[1]/input')
+	password_text_box.send_keys('testing4lyfe')
+	password_next_button = browser.find_element_by_xpath('//*[@id="passwordNext"]')
+	password_next_button.click()
+	time.sleep(5)
+
+	#checks if google wants us to verify via phone number
+	#if this happens we cant continue the scenario
+	google_beat_us = False
+	if (len(browser.window_handles) == 2): #if the sign in window is still open
+		google_beat_us = True
+	context.google_beat_us = google_beat_us
+	
+@then(u'we should see our name on the mobile nav bar')
+def step_impl(context):
+	google_beat_us = context.google_beat_us
+	browser = context.browser
+	if(not(google_beat_us)):
+		#we logged in!
+		original_window = browser.window_handles[0]
+		browser.switch_to_window(original_window)
+		upper_right_text = browser.find_element_by_xpath('//*[@id="mainNav"]/div/div[2]/div').text
+		assert upper_right_text == 'JOHN DOELogout'
+		time.sleep(3)
+	else:
+		#closing sign in
+		browser.close()
+		original_window = browser.window_handles[0]
+		browser.switch_to_window(original_window)
+		context.browser = browser
+
+@then(u'we should be able to log out on mobile')
 def step_impl(context):
 	google_beat_us = context.google_beat_us
 	browser = context.browser
