4689e5d319594efdadb2ca5cf387c140ab4dfcc5
mckennaman123 <mckennaman123@gmail.com>
2018-11-24 19:59:54 -0500

    importing same way for test_post and test_user to avoid helper error

--- Tests/unit_tests/test_get_all_posts.py
+++ Tests/unit_tests/test_get_all_posts.py
@@ -1,9 +1,10 @@
 #testing get all posts component
 import sys
 import os
+from shutil import copy2
 sys.path.insert(0, "../Server")
-from app import GetAllPosts
+#import app
 
 #copies test-db to server folder and changes name
-def setup():
-    os.
+#def setup():
+    #copy2('test-db.json', '../Server')
