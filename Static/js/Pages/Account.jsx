b4a1a4f0af9c64527d9d0869db4acffd5538d3e2
Daniel Loftus <dcloftus@hotmail.com>
2018-12-02 15:54:06 -0500

    wip

--- Static/js/Pages/Account.jsx
+++ Static/js/Pages/Account.jsx
@@ -5,17 +5,22 @@ export default class Account extends React.Component {
 
     constructor(props) {
         super(props);
+        console.log(this.props.user);
     }
 
     render() {
         return (
-            <div style={{ paddingTop: "90px" }}>
-                Account page!
+            <div style={{ paddingTop: "120px" }}>
+                <h2 class="Account-Name"> 
+                    {this.props.user.name}
+                </h2>
+
                 <Link to="/new-post">
-                    <button type="button">
-                        Create New Post
+                    <button type="button" class="button-make-new-post">
+                        Post New Route
                     </button>
                 </Link>
+                <hr class="hr-style"></hr>
             </div>
         );
     }
